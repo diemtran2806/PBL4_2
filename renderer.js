c6d6ad5a61433afc1e226f05810643d54abd385f
diemtran2806 <ttdiem2002@gmail.com>
2022-11-01 10:38:13 +0700

    show infor cpu

--- renderer.js
+++ renderer.js
@@ -1,7 +1,23 @@
-/**
- * This file is loaded via the <script> tag in the index.html file and will
- * be executed in the renderer process for that window. No Node.js APIs are
- * available in this process because `nodeIntegration` is turned off and
- * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
- * to expose Node.js functionality from the main process.
- */
+
+      const $ = document.querySelector.bind(document);
+      const $$ = document.querySelectorAll.bind(document);
+
+      const tabs = $$(".tab-item");
+      const panes = $$(".tab-pane");
+
+      const tabActive = $(".tab-item.active");
+      const line = $(".tabs .line");
+      line.style.left = tabActive.offsetLeft + "px";
+      line.style.width = tabActive.offsetWidth + "px";
+      tabs.forEach((tab, index) => {
+          const pane = panes[index];
+
+          tab.onclick = function () {
+              $(".tab-item.active").classList.remove("active");
+              $(".tab-pane.active").classList.remove("active");
+              line.style.left = this.offsetLeft + "px";
+              line.style.width = this.offsetWidth + "px";
+              this.classList.add("active");
+              pane.classList.add("active");
+          };
+      });
