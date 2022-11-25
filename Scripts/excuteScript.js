ac55e5c09d94c8e12d473889a96e22fe5f633ae0
diemtran2806 <ttdiem2002@gmail.com>
2022-11-25 14:06:50 +0700

    mem chart + disks

--- Scripts/excuteScript.js
+++ Scripts/excuteScript.js
@@ -1,16 +1,5 @@
 //import { getCPUInfo } from '../Scripts/loadScripts.js'
 const {getCPUInfo, getMemoryInfo} = require('../Scripts/loadScripts.js')
-// const renderItems = () => {
-//     return getCPUInfo.map((item, index) => {document.getElementById("cpu").innerText = item});
-// };
-// renderItems();
-// document.getElementById("cpu").innerText = getCPUInfo()[2];
-// let text = document.getElementById("cpu").innerText;
-
-// getCPUInfo().name.map(function(item,index) {        //call back
-//     //text+= item;
-//     document.getElementById("cpu").innerText += item[0];
-// })
 
 const name = getCPUInfo.name;
 const value = getCPUInfo.value;
@@ -21,8 +10,4 @@ for(var i=0; i<name.length-1; i++) {
              <td width="40%">${name[i]}</td>
              <td width="60%">${value[i]}</td>
         </tr>`;
-        // document.getElementById("cpu_value").innerHTML += `<li>${value[i]}</li>`;
-
-        // document.getElementById("cpu_name").innerText += name[i] + "\n";
-        // document.getElementById("cpu_value").innerText += value[i] + "\n";
 }
