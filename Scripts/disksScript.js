2b059d90e10ce8098f489efcf9b07bf588e45101
Cam <cam2362002@gmail.com>
2022-11-25 17:32:04 +0700

    update UI disks tab 😢

--- Scripts/disksScript.js
+++ Scripts/disksScript.js
@@ -1,15 +1,17 @@
 const { getDiskInfo } = require("../Scripts/loadScripts.js");
 
-const FileSystem = getDiskInfo.Filesystem;
-const Type = getDiskInfo.Type;
-const Blocks = getDiskInfo.Blocks;
-const Used = getDiskInfo.Used;
-const Available = getDiskInfo.Available;
-const UsePercent = getDiskInfo.UsePercent;
+const diskInfo = getDiskInfo;
+
+const FileSystem = diskInfo.Filesystem;
+const Type = diskInfo.Type;
+const Blocks = diskInfo.Blocks;
+const Used = diskInfo.Used;
+const Available = diskInfo.Available;
+const UsePercent = diskInfo.UsePercent;
 
 document.getElementById(
-    "headDisks"
-  ).innerHTML += `<tr>                               
+  "headDisks"
+).innerHTML += `<tr>                               
                 <th>Files System</th>
                 <th>Type</th>
                 <th>Total</th>     
@@ -18,20 +20,19 @@ document.getElementById(
                 <th>Used Percent</th>                   
         </tr>`;
 
-for(var i=0; i<FileSystem.length-1; i++) {
-        let id = "b"+i.toString()
-        document.getElementById("disksdata").innerHTML += 
-        `<tr>
+for (var i = 0; i < FileSystem.length - 1; i++) {
+  let id = "b" + i.toString();
+  document.getElementById("bodyDisks").innerHTML += `<tr>
              <td width="20%">${FileSystem[i]}</td>
              <td width="10%">${Type[i]}</td>
              <td width="10%">${Blocks[i]}</td>
              <td width="10%">${Used[i]}</td>
              <td width="10%">${Available[i]}</td>
              <td width="40%">
-                <div class="w3-light-grey">
-                    <div class="w3-container w3-purple w3-center" id =${id}>${UsePercent[i]}</div>
+                <div class="w3-light-grey w3-round">
+                    <div class="w3-container w3-round w3-purple" id =${id}>${UsePercent[i]}</div>
                 </div>
              </td>
         </tr>`;
-        document.getElementById(id).style.width = UsePercent[i];
+  document.getElementById(id).style.width = UsePercent[i];
 }
